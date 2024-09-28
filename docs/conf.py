60d13ce174f8748f4b1318286b11452bb3f42c94
iamleeon <165953114+iamleeon@users.noreply.github.com>
2024-09-28 03:09:03 +0300

    module 14

--- docs/conf.py
+++ docs/conf.py
@@ -1,15 +1,35 @@
-import sys
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
 import os
+import sys
+from dotenv import load_dotenv
 
-sys.path.append(os.path.abspath(".."))
-project = "Contacts REST API"
-copyright = "2024, iamleeon"
-author = "iamleeon"
+# Load environment variables from .env file
+load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
+sys.path.append(os.path.abspath('..'))
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
+project = 'Contacts REST API'
+copyright = '2024, iamleeon'
+author = 'iamleeon'
 
-templates_path = ["_templates"]
-exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-html_theme = "nature"
-html_static_path = ["_static"]
+extensions = ['sphinx.ext.autodoc']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'nature'
+html_static_path = ['_static']
